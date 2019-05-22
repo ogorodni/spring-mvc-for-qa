7a98214c51ce4fc321a899eddea78ec6dcf4238e
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2019-05-22 15:01:13 +0300

    Add JPA Repo operation.

--- src/main/java/com/acme/banking/dbo/spring/dao/AccountRepository.java
+++ src/main/java/com/acme/banking/dbo/spring/dao/AccountRepository.java
@@ -4,8 +4,11 @@ import com.acme.banking.dbo.spring.domain.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+
 /** In production will be used code-generated implementation by Spring-data-jpa module */
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
+    Collection<Account> findByEmail(String email);
     /** TODO Intro to Optional class */
 }
